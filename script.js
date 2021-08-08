f8e1f4a59a2a6d5ac1be4425103b57b04bcf76eb
Wilsonmfreire <62375886+Wilsonmfreire@users.noreply.github.com>
2021-08-08 19:34:09 -0300

    :package: entregando calculo de orçamento

--- script.js
+++ script.js
@@ -5,10 +5,33 @@ hamburguer.addEventListener("click", function(){
     document.querySelector(".container").classList.toggle("show-menu")
 })
 */
-
-
-
 document.querySelector(".hamburguer").addEventListener("click", ()=>
     document.querySelector(".container").classList.toggle("show-menu")
 );
+/**tratamento de evento */
+document.querySelector("#qtde").addEventListener("change", atualizarPreco)
+document.querySelector("#js").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-sim").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-nao").addEventListener("change", atualizarPreco)
+document.querySelector("#prazo").addEventListener("change", function(){
+    const prazo = document.querySelector("#prazo").value
+    document.querySelector("label[for=prazo]").innerHTML = `Prazo: ${prazo} semanas`
+    atualizarPreco()
+})
 
+
+function atualizarPreco() {
+    const qtde = document.querySelector("#qtde").value
+    const temJs = document.querySelector("#js").checked
+    const incluiLayout = document.querySelector("#layout-sim").checked
+    const prazo = document.querySelector("#prazo").value
+
+/*logica para calculo orcamento*/
+    let preco = qtde * 100;
+    if(temJs) preco *= 1.1
+    if (incluiLayout) preco += 500
+    let taxaUrgencia = 1 - prazo*0.1; 
+    preco *= 1 + taxaUrgencia
+    
+    document.querySelector("#preco").innerHTML = `R$ ${preco.toFixed(2)}`
+}
